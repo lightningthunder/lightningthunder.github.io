55c0ac7d6f3419ab75ad63ed5c142aff07cef0a3
mhmyes <lam.is@me.com>
2015-10-02 16:34:12 +0700

    el capitan

---
+++ module_footer.php
@@ -0,0 +1,126 @@
+<div class="container-fluid">
+    <div class="row" id="hero">
+      <div class="col-xs-12 no-side-padding clearfix">
+          <ul class="nav nav-tabs">
+            <li><a href="about.html">About us</a></li>
+            <li><a href="how.html">How it works</a></li>
+            <li><a href="impact.html">Social impact</a></li>
+            <li><a href="team.html">Our team</a></li>
+            <li><a href="careers.html">Careers</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-md-6 col-md-offset-3 padding-bottom-20">
+        <h1 class="col-xs-12 padding-top-20">We love traveling.</h1>
+        <p class="col-xs-12 lead">Traveling is one of the best ways to see the world in new perspectives. We believe that traveling of any kind or the tourism sector as a whole must head towards more collective responsibility and sustainable outlook. Local Alike works with local communities and businesses to create the best experiences for everybody.</p>
+      </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-md-6 col-md-offset-3 clearfix" id="about-scrollspy">
+          <ul class="nav nav-tabs text-center" data-offset-top="" data-spy="affix">
+            <li><a href="#intro">Who are we?</a></li>
+            <li><a href="#video">What's it like?</a></li>
+            <li><a href="#vision">Our vision</a></li>
+            <li><a href="#values">Values</a></li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-md-6 col-md-offset-3">
+          <div class="page-box" id="intro">
+            <div class="page-image">
+              <img src="../img/pages_team.jpg" />
+            </div>
+            <div class="page-content text-left clearfix">
+              <h2>LocalAlike.com is an online marketplace where you can find curated travel experiences in local communities across Southeast Asia.</h2>
+              <p class="lead">You get to experience trips that are designed to be enjoyable and authentic for every traveler and local alike. You simply have to try out the amazing food and drinks, adventurous treks, immersive cultures, exciting shows, and so much more that you won't find with traditional tour companies.</p>
+              <p class="lead">Local Alike matches you with local communities and responsible tour operators instead of simply matching you to one tour guide. This is called community based tourism, a form of tourism that allows local residents to be involved while emphasizing community development.</p>
+            </div>
+          </div>
+          <div class="page-box" id="video">
+            <div class="page-video">
+              <iframe width="560" height="auto" src="https://www.youtube.com/embed/PE3QTikx0JI" frameborder="0" allowfullscreen style="width: 100%;"></iframe>
+            </div>
+          </div>
+          <div class="page-box" id="vision">
+            <div class="page-content">
+              <div id="vision-statement" class="brdr-rds-3 text-center">
+                <span class="">Our vision</span>
+                <h3 class="">Create opportunities for locals communities and businesses to better their livelihoods through tourism.</h3>
+              </div>
+            </div>
+          </div>
+          <div class="page-box" id="values">
+            <div class="page-image">
+              <img src="../img/bangplub_food_5.jpg" />
+              <span id="web-url">www.localalike.com</span>
+            </div>
+            <div class="page-content clearfix">
+              <h2 class="col-xs-12 col-sm-10 col-sm-offset-1 text-center padding-bottom-20">Our website gives you instant access to curated travel experiences, in-depth content, unstaged photographs, social impact reports, real user reviews, and news to help you plan your trips better.</h2>
+              <ul class="fact-box-wrapper brdr-rds-3 list-unstyled clearfix">
+                <li class="col-xs-12 col-sm-6 fact-box-order">
+                  <div class="fact-box">
+                    <p>Large selection of responsible travel experiences</p>
+                  </div>
+                </li>
+                <li class="col-xs-12 col-sm-6 fact-box-order">
+                  <div class="fact-box">
+                    <p>Approved local hosts and tour operators</p>
+                  </div>
+                </li>
+                <li class="col-xs-12 col-sm-6 fact-box-order">
+                  <div class="fact-box">
+                    <p>All experiences contribute towards a social cause</p>
+                  </div>
+                </li>
+                <li class="col-xs-12 col-sm-6 fact-box-order">
+                  <div class="fact-box">
+                    <p>Secure, safe, and fast booking</p>
+                  </div>
+                </li> 
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+<div class="container-fluid global-footer">
+  <div class="row">
+    <div class="col-xs-12 col-sm-3">
+      <span><strong>Company</strong></span>
+      <ul class="list-unstyled">
+        <li>About Us</li>
+        <li>How it Works</li>
+        <li>Social Impact</li>
+        <li>The Team</li>
+        <li>Careers</li>
+      </ul>
+    </div>
+    <div class="col-xs-12 col-sm-3">
+      <span><strong>Support</strong></span>
+      <ul class="list-unstyled">
+        <li>Speak to a human</li>
+        <li>FAQ</li>
+        <li>Terms for Travelers</li>
+        <li>Terms for Hosts</li>
+        <li>Privacy</li>
+      </ul>
+    </div>
+    <div class="col-xs-12 col-sm-3">
+      <span><strong>Experiences</strong></span>
+      <ul class="list-unstyled">
+        <li>Curated Collections</li>
+        <li>Tours in Thailand</li>
+        <li>Bangkok day tours</li>
+        <li>Chiang Mai day tours</li>
+      </ul>
+    </div>
+    <div class="col-xs-12 col-sm-3">
+      <ul class="list-unstyled text-right right-column">
+        <li><img src="../img/la_logo_script_salmon-on-transparent.png" width="120" /></li>
+        <li>A Travel Social Enterprise</li>
+        <li>Designed in Bangkok</li>
+        <li>© 2015</li>
+      </ul>
+    </div>
+  </div>
+</div>
